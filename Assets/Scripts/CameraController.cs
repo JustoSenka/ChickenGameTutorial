bcdc2306c70feb3d6ddcd606242f4258baac945c
Justas <glodjus@gmail.com>
2019-12-07 17:45:35 +0200

    Adding initial project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject ToonChick;
+    public GameObject ToonChicken;
+    
+    public Transform GameObject;
+
+    public Vector3 lookOffset = new Vector3(0, 1, 0);
+    public float distance = 5f;//distance between camera and player position
+    public float cameraSpeed = 8f;//speed of camera
+    private float offset;
+
+    public void Start()
+    {
+        if (chickenIndex == 1)
+        {
+            GameObject = Instantiate(ToonChicken.Load("ToonChicken", typeof(GameObject))) as GameObject;
+        }
+
+        else (chickenIndex == 2){
+            GameObject = Instantiate(Toon Chick.Load("Toon Chick", typeof(GameObject))) as GameObject;
+
+        }
+    }
+
+    public void Update()
+    {
+        Vector3 offset = GameObject.position - transform.position;
+        Vector3 lookPosition = GameObject.position + lookOffset;
+        // look 360 degree
+        transform.LookAt(lookPosition);
+        if (Vector3.Distance(transform.position, lookPosition) > distance)
+        {
+            transform.Translate(0, 0, cameraSpeed * Time.deltaTime);
+        }
+        if (Vector3.Distance(transform.position, lookPosition) < 1f)
+        {
+            transform.position = new Vector3(0, 1, GameObject.position.z + 1f);
+        }
+    }
+}
+
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MainMenu : MonoBehaviour
+{
+    //public GameObject loadingImage;
+
+    public GameObject whiteChickenArrow;
+    public GameObject yellowChickenArrow;
+
+    public GameObject whiteChickenCollider;
+    public GameObject yellowChickenCollider;
+
+    public void Awake()
+   {
+        whiteChickenArrow.SetActive(false);
+        yellowChickenArrow.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            var chickenIndex = GetClickedChickenIndex();
+            Debug.Log(chickenIndex);
+
+            if (chickenIndex != 0)
+                SceneManager.LoadScene(1);
+        }
+    }
+
+    private int GetClickedChickenIndex()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Debug.Log("Raycast hit");
+
+            if (hit.collider.gameObject == whiteChickenCollider)
+                return 1;
+            else if (hit.collider.gameObject == yellowChickenCollider)
+                return 2;
+            else
+                return 0;
+        }
+
+        Debug.Log("No hit");
+        return 0;
+    }
+
+    public void StartLevel(int level)
+    {
+        whiteChickenArrow.SetActive(true);
+        yellowChickenArrow.SetActive(true);
+
+        // SceneManager.LoadScene(level);
+    }
+
+    public void ShowOptions()
+    {
+
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+    }
+}
+
