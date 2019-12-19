fc39a99b5556056d36b9259942dbd64ec6610e4e
Justas <glodjus@gmail.com>
2019-12-19 02:17:36 +0200

    fixing if def for build

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-#if UNITY_EDITOR
-using UnityEditor;
 using UnityEngine.UI;
 
+#if UNITY_EDITOR
+using UnityEditor;
 #endif
 
 public class MainMenu : MonoBehaviour
